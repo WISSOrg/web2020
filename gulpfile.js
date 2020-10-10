bbc969290e3f00f5bed7514d17adbce486f71195
Yuki Koyama <yuki@koyama.xyz>
2020-10-10 17:00:16 +0900

    Add other organizing committee members

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,17 @@ const sass = require("gulp-sass");
 const yaml = require("js-yaml")
 
 const menu_data_path = "src/data/menu.yml";
+const organizing_committee_data_path = "src/data/organizing-committee.yml"
 
 gulp.task('pug', function(done) {
     gulp.src("src/*.pug")
         .pipe(data(function(file) {
-            yaml_data = yaml.load(fs.readFileSync(menu_data_path));
-            return { menu_data: yaml_data };
+            yaml_menu_data = yaml.load(fs.readFileSync(menu_data_path));
+            yaml_organizing_committee_data = yaml.load(fs.readFileSync(organizing_committee_data_path));
+            return {
+                menu_data: yaml_menu_data,
+                organizing_committee_data: yaml_organizing_committee_data
+            };
         }))
         .pipe(pug({ pretty: true }))
         .pipe(gulp.dest("dst"));
